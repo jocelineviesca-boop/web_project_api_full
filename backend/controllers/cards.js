b78a078edc1fb6f5a8d9194c3f3e0437076459b2
JocelineTripleTen <jocelineviesca@gmail.com>
2026-09-11 17:49:09 -0600

    Ajustar respuestas: arreglo directo para cards y objeto data para users

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -2,7 +2,7 @@ const Card = require('../models/card');
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => res.send(cards))
     .catch(next);
 };
 
